3ce5699c219f2530d303710336da6e39dd9bdf85
MagmaDevil <207029261+MagmaDevil@users.noreply.github.com>
2025-07-01 21:44:27 +0900

    [2025.7.1] 선형 컨테이너

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -1,8 +1,87 @@
 ﻿#include <iostream>
+#include <vector>
+#include <list>
 using namespace std;
 
 int main()
 {
+#pragma region 선형 컨테이너
+
+	// 데이터를 선형으로 저장, 특별한 제약이나 규칙이 없다
+
+#pragma region vector container
+
+	//vector<int> vector;
+	//
+	//vector.reserve(10);
+	//
+	//vector.push_back(10);
+	//
+	//cout << "size: " << vector.size() << endl;
+	//cout << "capacity: " << vector.capacity() << endl;
+	//
+	//vector.push_back(20);
+	//
+	//cout << "size: " << vector.size() << endl;
+	//cout << "capacity: " << vector.capacity() << endl;
+	//
+	//vector.push_back(30);
+	//
+	//cout << "size: " << vector.size() << endl;
+	//cout << "capacity: " << vector.capacity() << endl;
+	//
+	//vector.push_back(40);
+	//
+	//cout << "size: " << vector.size() << endl;
+	//cout << "capacity: " << vector.capacity() << endl;
+	//
+	//vector.push_back(50);
+	//
+	//cout << "size: " << vector.size() << endl;
+	//cout << "capacity: " << vector.capacity() << endl;
+	//
+	//vector.push_back(60);
+	//
+	//cout << "size: " << vector.size() << endl;
+	//cout << "capacity: " << vector.capacity() << endl;
+	//
+	//vector.push_back(70);
+	//
+	//cout << "size: " << vector.size() << endl;
+	//cout << "capacity: " << vector.capacity() << endl;
+	//
+	//vector.push_back(80);
+	//
+	//cout << "size: " << vector.size() << endl;
+	//cout << "capacity: " << vector.capacity() << endl;
+	//
+	//vector.push_back(90);
+	//
+	//cout << "size: " << vector.size() << endl;
+	//cout << "capacity: " << vector.capacity() << endl;
+	//
+	//vector.push_back(100);
+	//
+	//cout << "size: " << vector.size() << endl;
+	//cout << "capacity: " << vector.capacity() << endl;
+	//
+	//vector.pop_back();
+	//
+	//for (int i = 0; i < vector.size(); i++)
+	//{
+	//	cout << "vector: " << vector[i] << endl;
+	//}
+	
+#pragma endregion
+
+#pragma region list container
+
+	
+
+#pragma endregion
+
+
+#pragma endregion
 
 	return 0;
 }
